5e3ff1a187b1b45b5805b5e83090e98183b9bff9
Ryan Dougherty <ryan.dougherty@asu.edu>
2017-04-18 13:47:05 -0700

    Added Ingredient.java

--- app/src/main/java/com/schachte/android/proactive_food_app/util/GTINToIngredientParser.java
+++ app/src/main/java/com/schachte/android/proactive_food_app/util/GTINToIngredientParser.java
@@ -1,8 +1,14 @@
 package com.schachte.android.proactive_food_app.util;
 
+import com.schachte.android.proactive_food_app.models.Ingredient;
+
 /**
  * Created by Ryan on 4/18/17.
  */
 
 public class GTINToIngredientParser {
+    public static Ingredient convertGTINIdToIngredient(String gtin) {
+        Ingredient i = new Ingredient();
+        return i;
+    }
 }
