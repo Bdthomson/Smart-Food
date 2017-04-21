6c3da3fcdf12f230e82f9d27893653f63f2ff738
Ryan Dougherty <ryan.dougherty@asu.edu>
2017-04-21 15:11:44 -0700

    Preliminary ImageView for ManualIngredient, still not loading image from disk (but can pick one...)

--- app/src/main/java/com/schachte/android/proactive_food_app/activities/add_ingredient_activities/ManualIngredientActivity.java
+++ app/src/main/java/com/schachte/android/proactive_food_app/activities/add_ingredient_activities/ManualIngredientActivity.java
@@ -1,21 +1,36 @@
 package com.schachte.android.proactive_food_app.activities.add_ingredient_activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.schachte.android.proactive_food_app.R;
 import com.schachte.android.proactive_food_app.database.DataAccessLayer;
 import com.schachte.android.proactive_food_app.models.Ingredient;
+import com.squareup.picasso.Picasso;
+
+import java.io.ByteArrayOutputStream;
 
 public class ManualIngredientActivity extends Activity {
 
     private EditText normalName;
     private EditText generalName;
+
+    private ImageView chosenImageView;
+    private Button loadChosenImageButton;
+
+    private String imageURL = "none";
+
+    private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +47,20 @@ public class ManualIngredientActivity extends Activity {
 
         normalName = (EditText)findViewById(R.id.nameOfIngredient);
         generalName = (EditText)findViewById(R.id.manualGeneralTextView);
+
+        chosenImageView = (ImageView)findViewById(R.id.manualImageView);
+        chosenImageView.setDrawingCacheEnabled(true);
+        chosenImageView.buildDrawingCache(true);
+
+        loadChosenImageButton = (Button)findViewById(R.id.manualLoadImageButton);
+
+        loadChosenImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
+            }
+        });
 
         normalName.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -56,6 +85,16 @@ public class ManualIngredientActivity extends Activity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK) {
+            imageURL = data.getData().toString();
+            Picasso.with(this).load(data.getData()).into(chosenImageView);
+        }
+    }
+
     private void addIngredient() {
         Log.i(normalName.getText().toString(), normalName.getText().toString());
         if (normalName.getText().length() == 0) {
@@ -70,11 +109,28 @@ public class ManualIngredientActivity extends Activity {
             toAdd.setIngredientGeneralName(generalNameOfIngredient);
 
             toAdd.setIngredientId(0);
-            toAdd.setIngredientImageBytes("none");
-            toAdd.setIngredientImageURL("none");
+
+            toAdd.setIngredientImageBytes("none"); // as a default
+            if (chosenImageView != null) {
+
+                chosenImageView.buildDrawingCache();
+                Bitmap bitmap = chosenImageView.getDrawingCache();
+                if (bitmap != null) {
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+                    byte[] bytes = baos.toByteArray();
+                    String base64 = null;
+                    base64 = Base64.encodeToString(bytes, 0);
+
+                    toAdd.setIngredientImageBytes(base64);
+                }
+            }
+
+            toAdd.setIngredientImageURL(imageURL);
 
             DataAccessLayer dal = new DataAccessLayer(this);
             dal.storeIngredient(toAdd);
+
             finish();
         }
     }
